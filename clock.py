67584ef1d33ad15771a8b4397618bb2a3af14aef
Zengfeng Zhang <53667689+zhangshanbeng@users.noreply.github.com>
2021-09-23 17:47:29 +0800

    Update clock.py
    修复了filldate逻辑错误的问题

--- clock.py
+++ clock.py
@@ -6,9 +6,18 @@ EHALL_USER = ''
 EHALL_PWD = ''
 PHONE_NUMBER = ''
 
+import json
+from licsber.auth.wisedu import get_wisedu_session
+
+#信息门户账号、密码，手机号
+EHALL_USER = ''
+EHALL_PWD = ''
+PHONE_NUMBER = ''
+
 LOGIN_URL = 'http://authserver.njit.edu.cn/authserver/login?service=http%3A%2F%2Fehall.njit.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.njit.edu.cn%2Fnew%2Findex.html'
 GET_URL = 'http://ehallapp.njit.edu.cn/publicapp/sys/lwNjitHealthInfoDailyClock/index.do#/healthClock'
 QUERY_URL = 'http://ehallapp.njit.edu.cn/publicapp/sys/lwNjitHealthInfoDailyClock/modules/healthClock/V_LWPUB_JKDK_QUERY.do'
+TIME_URL = 'http://ehallapp.njit.edu.cn/publicapp/sys/lwpub/api/getServerTime.do'
 SAVE_URL = 'http://ehallapp.njit.edu.cn/publicapp/sys/lwNjitHealthInfoDailyClock/modules/healthClock/T_HEALTH_DAILY_INFO_SAVE.do'
 
 s = get_wisedu_session(LOGIN_URL, EHALL_USER, EHALL_PWD)
@@ -22,13 +31,17 @@ user_name = info_dict['USER_NAME']
 dept_code = info_dict['DEPT_CODE']
 dept_name = info_dict['DEPT_NAME']
 
+res = s.post(TIME_URL)
+date = json.loads(res.text)['date']
+date = str(date).replace('/','-')
+
 data_am = {
     "USER_ID":user_id,
     "USER_NAME":user_name,
     "DEPT_CODE":dept_code,
     "DEPT_NAME":dept_name,
     "PHONE_NUMBER":PHONE_NUMBER,
-    "FILL_TIME":"2021-09-23 08:14:23",
+    "FILL_TIME":date,
     "BY3": "001",
     "CLOCK_SITUATION":"江苏省南京市江宁区南京工程学院",
     "TODAY_SITUATION":"001",
@@ -41,7 +54,6 @@ data_am = {
 
 data_pm = data_am.copy()
 data_pm['BY3'] = '002'
-data_pm['FILL_TIME'] = '2021-09-23 12:57:43'
 
 res = s.post(SAVE_URL, data=data_pm)
 print(res.text)
