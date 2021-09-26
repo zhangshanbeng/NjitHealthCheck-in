d03c1709ceb68a252c9b5e163859aa1cf7f224a7
Zengfeng Zhang <53667689+zhangshanbeng@users.noreply.github.com>
2021-09-26 10:59:19 +0800

    V2.0: webvpn url

---
+++ webvpn.py
@@ -0,0 +1,73 @@
+from Crypto.Cipher import AES
+from binascii import hexlify, unhexlify
+
+key_ = b'wrdvpnisthebest!'
+iv_  = b'wrdvpnisthebest!'
+institution = 'webvpn.njit.edu.cn'   # Change the hostname here like 'webvpn.xxx.edu.cn'
+
+def getCiphertext(plaintext, key = key_, cfb_iv = iv_, size = 128):
+    '''From plantext hostname to ciphertext'''
+
+    message = plaintext.encode('utf-8')
+
+    cfb_cipher_encrypt = AES.new(key, AES.MODE_CFB, cfb_iv, segment_size = size)       # Must include segment_size
+    mid = cfb_cipher_encrypt.encrypt(message)
+
+    return hexlify(mid).decode()
+
+
+def getPlaintext(ciphertext, key = key_, cfb_iv = iv_, size = 128):
+    '''From ciphertext hostname to plaintext'''
+
+    message = unhexlify(ciphertext.encode('utf-8'))
+
+    cfb_cipher_decrypt = AES.new(key, AES.MODE_CFB, cfb_iv, segment_size = size)
+    cfb_msg_decrypt = cfb_cipher_decrypt.decrypt(message).decode('utf-8')
+
+    return cfb_msg_decrypt
+
+    return message
+
+def getVPNUrl(url):
+    '''From ordinary url to webVPN url'''
+
+    parts = url.split('://')
+    pro = parts[0]
+    add = parts[1]
+
+    hosts = add.split('/')
+    cph = getCiphertext(hosts[0])
+    fold = '/'.join(hosts[1:])
+
+    key = hexlify(iv_).decode('utf-8')
+
+    return 'https://' + institution + '/' + pro + '/' + key + cph + '/' + fold
+
+def getOrdinaryUrl(url):
+    '''From webVPN url to ordinary url'''
+
+    parts = url.split('/')
+    pro = parts[3]
+    key_cph = parts[4]
+
+    if key_cph[:16] == hexlify(iv_).decode('utf-8'):
+        print(key_cph[:32])
+        return None
+    else:
+        hostname = getPlaintext(key_cph[32:])
+        fold = '/'.join(parts[5:])
+
+        return pro + "://" + hostname + '/' + fold
+
+if __name__ == '__main__':
+
+    #直观体验一下
+    #转vpn_url
+    url = 'http://202.119.161.130/'
+    print('From ordinary url: \n' + getVPNUrl(url))
+    #https://webvpn.njit.edu.cn/http/77726476706e69737468656265737421a2a713d27661311e2f5ed8e2c90673/
+
+    #还原url
+    VPNUrl = 'https://' + institution + '/https/77726476706e69737468656265737421fbf952d2243e635930068cb8/KCMS/detail/detail.aspx?dbcode=CJFQ&dbname=CJFD2007&filename=JEXK200702000&uid=WEEvREcwSlJHSldRa1FhcTdnTnhXY20wTWhLQWVGdmJFOTcvMFFDWDBycz0=$9A4hF_YAuvQ5obgVAqNKPCYcEjKensW4IQMovwHtwkF4VYPoHbKxJw!!&v=MTYzNjU3cWZaT2RuRkNuaFZMN0tMeWpUWmJHNEh0Yk1yWTlGWklSOGVYMUx1eFlTN0RoMVQzcVRyV00xRnJDVVI='
+    print('\nFrom webVPN url: \n' + getOrdinaryUrl(VPNUrl))
+    #https://kns.cnki.net/KCMS/detail/detail.aspx?dbcode=CJFQ&dbname=CJFD2007&filename=JEXK200702000&uid=WEEvREcwSlJHSldRa1FhcTdnTnhXY20wTWhLQWVGdmJFOTcvMFFDWDBycz0=$9A4hF_YAuvQ5obgVAqNKPCYcEjKensW4IQMovwHtwkF4VYPoHbKxJw!!&v=MTYzNjU3cWZaT2RuRkNuaFZMN0tMeWpUWmJHNEh0Yk1yWTlGWklSOGVYMUx1eFlTN0RoMVQzcVRyV00xRnJDVVI=
