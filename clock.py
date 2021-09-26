d90c3207afc66125a040d81ac31d1022064d4c4a
Zengfeng Zhang <53667689+zhangshanbeng@users.noreply.github.com>
2021-09-26 10:57:16 +0800

    V2.0: 加入webvpn访问

--- clock.py
+++ clock.py
@@ -1,10 +1,17 @@
 import json
-from licsber.auth.wisedu import get_wisedu_session
+from licsber.wisedu.get_session import *
+from licsber.spider import get_session
+from vpn import NJITwebvpn
+from webvpn import getVPNUrl
 
-#信息门户账号、密码，手机号
+#信息门户账号,密码,手机号 (必填)
 EHALL_USER = ''
 EHALL_PWD = ''
 PHONE_NUMBER = ''
+
+s = get_session()
+vpn = NJITwebvpn(EHALL_USER,EHALL_PWD)
+s = vpn.login()
 
 LOGIN_URL = 'http://authserver.njit.edu.cn/authserver/login?service=http%3A%2F%2Fehall.njit.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.njit.edu.cn%2Fnew%2Findex.html'
 GET_URL = 'http://ehallapp.njit.edu.cn/publicapp/sys/lwNjitHealthInfoDailyClock/index.do#/healthClock'
@@ -12,10 +19,22 @@ QUERY_URL = 'http://ehallapp.njit.edu.cn/publicapp/sys/lwNjitHealthInfoDailyCloc
 TIME_URL = 'http://ehallapp.njit.edu.cn/publicapp/sys/lwpub/api/getServerTime.do'
 SAVE_URL = 'http://ehallapp.njit.edu.cn/publicapp/sys/lwNjitHealthInfoDailyClock/modules/healthClock/T_HEALTH_DAILY_INFO_SAVE.do'
 
-s = get_wisedu_session(LOGIN_URL, EHALL_USER, EHALL_PWD)
-s.get(GET_URL)
+URL_DICT = { 'LOGIN_URL': LOGIN_URL, 'GET_URL': GET_URL, 'QUERY_URL': QUERY_URL, 'TIME_URL': TIME_URL, 'SAVE_URL': SAVE_URL }
 
-res = s.post(QUERY_URL)
+#套了一层webvpn进行加速，如果不需要就注释掉
+#webvpn: 伪内网访问
+for k,v in URL_DICT.items():
+    URL_DICT[k] = getVPNUrl(v)
+
+s = get_wisedu_session_webvpn(URL_DICT['LOGIN_URL'], EHALL_USER, EHALL_PWD, s)
+
+#公网访问(二选一)
+#s = get_wisedu_session(URL_DICT['LOGIN_URL'], EHALL_USER, EHALL_PWD)
+
+s.get(URL_DICT['GET_URL'])
+
+#查询个人信息: 学号, 姓名, 院系编号, 学院名称
+res = s.post(URL_DICT['QUERY_URL'])
 info_dict = json.loads(res.text)['datas']['V_LWPUB_JKDK_QUERY']['rows'][0]
 
 user_id = info_dict['USER_ID']
@@ -23,10 +42,14 @@ user_name = info_dict['USER_NAME']
 dept_code = info_dict['DEPT_CODE']
 dept_name = info_dict['DEPT_NAME']
 
-res = s.post(TIME_URL)
+#获取服务器时间，可改为本地时间
+#时间格式: 2021-09-26 09:51:06
+res = s.post(URL_DICT['TIME_URL'])
 date = json.loads(res.text)['date']
 date = str(date).replace('/','-')
 
+#data_am: 上午提交的信息; data_pm: 下午提交的信息
+#BY3字段决定了打卡类型, 001:晨间打卡, 002:晚间打卡
 data_am = {
     "USER_ID":user_id,
     "USER_NAME":user_name,
@@ -47,5 +70,9 @@ data_am = {
 data_pm = data_am.copy()
 data_pm['BY3'] = '002'
 
-res = s.post(SAVE_URL, data=data_pm)
+#上午打卡
+res = s.post(URL_DICT['SAVE_URL'], data=data_am)
+#下午打卡
+#res = s.post(URL_DICT['SAVE_URL'], data=data_pm)
+
 print(res.text)
